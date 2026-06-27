db2aab15474cd5004d0fd9870e5ff448fd229894
Justin Su <injustsu@gmail.com>
2026-06-26 20:22:27 -0400

    Clean up `_autopair_tab` function
    - Use `\S` regex character class
    - Remove redundant `commandline --function` option

--- functions/_autopair_tab.fish
+++ functions/_autopair_tab.fish
@@ -1,7 +1,7 @@
 function _autopair_tab
     commandline --paging-mode && down-or-search && return
 
-    string match --quiet --regex -- '\$[^\s]*"$' (commandline --current-token) &&
-        commandline --function end-of-line --function backward-delete-char
+    string match --quiet --regex -- '\$\S*"$' (commandline --current-token) &&
+        commandline --function end-of-line backward-delete-char
     commandline --function complete
 end
